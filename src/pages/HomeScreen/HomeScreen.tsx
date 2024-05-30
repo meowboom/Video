58425dbaf0d5793296f530644012e3d4b34108b9
Maksim Dzinisiuk <101088991+meowboom@users.noreply.github.com>
2024-05-30 22:44:37 +0200

    add to storage, create users fetch, create users storage, littel correct in code

--- src/pages/HomeScreen/HomeScreen.tsx
+++ src/pages/HomeScreen/HomeScreen.tsx
@@ -6,7 +6,6 @@ import RightBar from "../../widget/RightBar/RightBar";
 type Props = { children?: ReactNode };
 
 const HomeScreen = ({ children }: Props) => {
-
   return (
     <div className="h-full w-full ">
       <Header />
